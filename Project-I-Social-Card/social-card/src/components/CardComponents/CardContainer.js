498b22d2f29b40ccc828d9d62da4cefac174abd8
josh cooter <thejoshcooter@gmail.com>
2019-04-22 10:54:38 -0500

    add CardContent component to complete components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from './CardBanner.js';
+import CardContent from './CardContent.js';
 
 const CardContainer = props => {
     return (
         <div>
             <CardBanner />
+            <CardContent />
         </div>
     );
 };
