1993987696eac22566848cbbc645d75de4f98070
josh cooter <thejoshcooter@gmail.com>
2019-04-22 10:46:12 -0500

    fix HeaderContainer component to contain other header components

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.css';
-import Thumbnail from './components/HeaderComponents/ImageThumbnail.js'
-import Title from './components/HeaderComponents/HeaderTitle.js';
-import HeaderContent from './components/HeaderComponents/HeaderContent';
+import HeaderContainer from './components/HeaderComponents/HeaderContainer.js';
 
 
 
 const App = () => {
   return (
     <div>
-      <Thumbnail />
-      <Title />
-      <HeaderContent />
+      <HeaderContainer />
       <h3>Welcome to React Social Card!</h3>
       <p>
         Begin by exploring the `components` directory. You'll notice we have a
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import Thumbnail from './ImageThumbnail';
+import Title from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = props => {
+    return (
+        <div>
+            <Thumbnail />
+            <Title />
+            <HeaderContent />
+        </div>
+    );
+};
+
+export default HeaderContainer
