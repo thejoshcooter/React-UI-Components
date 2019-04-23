f807ce77c4112002cc17b452b6f51ecee4116553
josh cooter <thejoshcooter@gmail.com>
2019-04-23 18:08:20 -0500

    complete calculator mvp

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="wrapper">
+      <CalculatorDisplay />
+      <div className="button-row">
+        <ActionButton text="clear" />
+        <NumberButton number="/" className="one-col red" />
+      </div>
+      <div className="button-row">
+        <NumberButton number="7" className="one-col" />
+        <NumberButton number="8" className="one-col" />
+        <NumberButton number="9" className="one-col" />
+        <NumberButton number="x" className="one-col red" />
+      </div>
+      <div className="button-row">
+        <NumberButton number="4" className="one-col" />
+        <NumberButton number="5" className="one-col" />
+        <NumberButton number="6" className="one-col" />
+        <NumberButton number="-" className="one-col red" />
+      </div>
+      <div className="button-row">
+        <NumberButton number="1" className="one-col" />
+        <NumberButton number="2" className="one-col" />
+        <NumberButton number="3" className="one-col" />
+        <NumberButton number="+" className="one-col red" />
+      </div>
+      <div className="button-row">
+        <ActionButton text="0" />
+        <NumberButton number="=" className="one-col red" />
+      </div>
     </div>
   );
 };
